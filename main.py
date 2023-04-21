09a665c8ea3e0e209fd457a5f7d5e63f6433222e
kraus-013 <127904784+kraus-013@users.noreply.github.com>
2023-04-21 16:08:04 -0500

    Fixed all the problems caused when merging with main

--- main.py
+++ main.py
@@ -37,40 +37,13 @@ def get_request(zip):
 # Efficiency is the efficiency of the solar panel (usually given as a percentage) (E.g. 19% = 0.19)
 # Time is the time duration for which the solar panel is exposed to the sun in hours
 
-annual_Energy = annual_avg_dni * 0.5471 * 0.22 * 365 # the *0.75 could be omitted. I'm not sure.
 
-grid_electricity_cost = 0.1409 #Cents per Kwh
-cost_of_system = 124.99+439.99 #Cost of total installation
-annual_cost_savings = round(annual_Energy*grid_electricity_cost,2)
-payback_years = round(cost_of_system/annual_cost_savings,2)
 
-#print(annual_avg_dni)
 
-print("The average annual solar energy generated for latitude: " + lat + " and longitude: " + lon + " is " + str(round(annual_Energy,4)) + " kWh")
-
-print("The cost savings from this system could be as much as $" + str(annual_cost_savings) + " Per Year" )
-
-print("The payback period could be as little as " + str(payback_years) + " years.")
-
-monthly_dni = data["outputs"]["avg_dni"]["monthly"]
-monthly_ghi = data["outputs"]["avg_ghi"]["monthly"]
-
-#Bar graphs are formatted as {'key': value}
-#print(monthly_dni)
-annual_returns = [annual_cost_savings-cost_of_system]
-years = [1]
-for i in range (round(payback_years)+5):
-    annual_returns.append(annual_cost_savings*i-cost_of_system)
-    years.append(i)
-
-figures, axes = plt.subplots(2,2)
-axes[0][0].bar(monthly_dni.keys(), monthly_dni.values())
-axes[0][1].bar(monthly_ghi.keys(), monthly_ghi.values())
-axes[1][0].bar(years, annual_returns)
 monthly_dni = None
 monthly_ghi = None
 
-figure, axes = plt.subplots(1,2)
+figure, axes = plt.subplots(2,2)
 
 rect1 = None
 rect2 = None
@@ -81,14 +54,38 @@ def redraw():
     global data
     data = get_request(addr)
     annual_avg_dni = float(data['outputs']['avg_dni']['annual'])
-    annual_Energy = annual_avg_dni * 1 * 0.2
+    annual_Energy = annual_avg_dni * 0.5471 * 0.22 * 365 # the *0.75 could be omitted. I'm not sure.
     print("The average annual solar energy generated for zip code " + addr + " is " + str(annual_Energy) + " kWh")
+    grid_electricity_cost = 0.1409 #Cents per Kwh
+    cost_of_system = 124.99+439.99 #Cost of total installation
+    annual_cost_savings = round(annual_Energy*grid_electricity_cost,2)
+    payback_years = round(cost_of_system/annual_cost_savings,2)
+
+    #print(annual_avg_dni)
+
+    print("The average annual solar energy generated for latitude: " + lat + " and longitude: " + lon + " is " + str(round(annual_Energy,4)) + " kWh")
+
+    print("The cost savings from this system could be as much as $" + str(annual_cost_savings) + " Per Year" )
+
+    print("The payback period could be as little as " + str(payback_years) + " years.")
+
     monthly_dni = data["outputs"]["avg_dni"]["monthly"]
     monthly_ghi = data["outputs"]["avg_ghi"]["monthly"]
+
+    #Bar graphs are formatted as {'key': value}
+    #print(monthly_dni)
+    annual_returns = [annual_cost_savings-cost_of_system]
+    years = [1]
+    for i in range (round(payback_years)+5):
+        annual_returns.append(annual_cost_savings*i-cost_of_system)
+        years.append(i)
+
+    
+    axes[1][0].bar(years, annual_returns, color="#2596be")
     
     if rect1 is None or rect2 is None:
-        rect1 = axes[0].bar(monthly_dni.keys(), monthly_dni.values())
-        rect2 = axes[1].bar(monthly_ghi.keys(), monthly_ghi.values())
+        rect1 = axes[0][0].bar(monthly_dni.keys(), monthly_dni.values())
+        rect2 = axes[0][1].bar(monthly_ghi.keys(), monthly_ghi.values())
     else:
         for rect, h in zip(rect1, monthly_dni.values()):
             rect.set_height(h)
@@ -104,7 +101,7 @@ def update(zip):
     data = get_request(addr)
     redraw()
 
-update("56387")
+update("56301")
 
 
 figure.subplots_adjust(bottom = 0.2)
