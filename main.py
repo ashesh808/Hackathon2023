dbec0bc53135396c317a6db9c8a1c80f6baddf54
Ashesh Nepal <asheshusa9@gmail.com>
2023-04-21 18:09:04 -0500

    Added cost saving function

--- main.py
+++ main.py
@@ -6,18 +6,6 @@ lat = "40"
 lon = "-105"
 zipcode = "56387"
 
-
-# Energy = DNI x Area x Efficiency x Time
-# Where:
-
-# DNI is the Direct Normal Irradiance in Kwh/M^2/day
-# Area is the surface area of the solar panel in m^2
-# Efficiency is the efficiency of the solar panel (usually given as a percentage) (E.g. 19% = 0.19)
-# Time is the time duration for which the solar panel is exposed to the sun in hours
-
-
-
-
 monthly_dni = None
 monthly_ghi = None
 
@@ -32,6 +20,21 @@ ax=figure.add_axes([0.1, 0.05, 0.8, 0.075])
 ta=ax.text(0,-0.5, "Annual Solar Generation: "+ str(annual_Energy) + " kWh")
 tb=ax.text(0.35,-0.5, "Annual Cost Savings: $"+ str(annual_cost_savings))
 
+
+def cost_saving():
+    grid_electricity_cost = 0.1409 #Cents per Kwh
+    cost_of_system = 124.99+439.99 #Cost of total installation
+    annual_cost_savings = round(annual_Energy*grid_electricity_cost,2)
+    payback_years = round(cost_of_system/annual_cost_savings,2)
+    print("The cost savings from this system could be as much as $" + str(annual_cost_savings) + " Per Year" )
+    print("The payback period could be as little as " + str(payback_years) + " years.")
+    annual_returns = [annual_cost_savings-cost_of_system]
+    years = [1]
+    for i in range (round(payback_years)+5):
+        annual_returns.append(annual_cost_savings*i-cost_of_system)
+        years.append(i)
+    axes[2].bar(years, annual_returns, color="#2596be")
+
 def redraw():
     global rect1
     global rect2
@@ -44,33 +47,11 @@ def redraw():
     annual_avg_dni = float(data['outputs']['avg_dni']['annual'])
     annual_Energy = annual_avg_dni * 0.5471 * 0.22 * 365 # the *0.75 could be omitted. I'm not sure.
     print("The average annual solar energy generated for zip code " + zipcode + " is " + str(annual_Energy) + " kWh")
-    grid_electricity_cost = 0.1409 #Cents per Kwh
-    cost_of_system = 124.99+439.99 #Cost of total installation
-    annual_cost_savings = round(annual_Energy*grid_electricity_cost,2)
-    payback_years = round(cost_of_system/annual_cost_savings,2)
-
-    #print(annual_avg_dni)
-
     print("The average annual solar energy generated for latitude: " + lat + " and longitude: " + lon + " is " + str(round(annual_Energy,4)) + " kWh")
-
-    print("The cost savings from this system could be as much as $" + str(annual_cost_savings) + " Per Year" )
-
-    print("The payback period could be as little as " + str(payback_years) + " years.")
-
+    cost_saving()
     monthly_dni = data["outputs"]["avg_dni"]["monthly"]
     monthly_ghi = data["outputs"]["avg_ghi"]["monthly"]
 
-    #Bar graphs are formatted as {'key': value}
-    #print(monthly_dni)
-    annual_returns = [annual_cost_savings-cost_of_system]
-    years = [1]
-    for i in range (round(payback_years)+5):
-        annual_returns.append(annual_cost_savings*i-cost_of_system)
-        years.append(i)
-
-    
-    axes[2].bar(years, annual_returns, color="#2596be")
-    
     if rect1 is None or rect2 is None:
         rect1 = axes[0].bar(monthly_dni.keys(), monthly_dni.values())
         rect2 = axes[1].bar(monthly_ghi.keys(), monthly_ghi.values())
