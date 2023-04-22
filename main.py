4c391156b423da6e4e8bbb5e9c91026d9a9106e2
kraus-013 <127904784+kraus-013@users.noreply.github.com>
2023-04-21 21:05:38 -0500

    Modified the Clare branch with the code that was needed from this branch

--- main.py
+++ main.py
@@ -1,36 +1,34 @@
 import matplotlib.pyplot as plt
 from matplotlib.widgets import TextBox
 import solar_data
-import json
-import math
+
+# Creates a grid layout format for the buttons and graphs
+gs = plt.GridSpec(nrows=12, ncols=2, height_ratios=[8, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0], figure=None)
+figure = plt.figure()
+
+text_subplot1 = figure.add_subplot(gs[10, :])
+text_subplot1.axis('off')
+text_subplot2 = figure.add_subplot(gs[11, :])
+text_subplot2.axis('off')
+
+# Holds all the input variables that are used in calculations and graphing
+input_vars = {'zipcode': None, 'surfaceArea': None, 'powerRating': None, 'efficiency': None, 'cost': None, 'time': None}
+
+# TEMP: Set default value for zip code
+input_vars['zipcode'] = "56387"
 
 lat = "40"
 lon = "-105"
-lat = "45.465135"# St. Cloud MN
-lon = "-94.251555"#St. Cloud MN
-
-addr = "56301" #zip code for requesting solar data that way
-response = requests.get(base_url + "api/solar/solar_resource/v1.json?api_key=DEMO_KEY&address=" + addr)
-
-#Requesting solar data via latitude and longitude
-#response = requests.get(base_url + "api/solar/solar_resource/v1.json?api_key=DEMO_KEY&lat=" + lat + "&lon=" + lon)
-
 
 # Energy = DNI x Area x Efficiency x Time
 # Where:
 #test
 # DNI is the Direct Normal Irradiance in W/m^2
 # Area is the surface area of the solar panel in m^2
-# Efficiency is the efficiency of the solar panel (usually given as a percentage) (E.g. 19% = 0.19)
-# Time is the time duration for which the solar panel is exposed to the sun in hours
-
-
-
+# Efficiency is the efficiency of the solar panel (usually given as a percentage)
 
 monthly_dni = None
 monthly_ghi = None
-
-figure, axes = plt.subplots(1,3)
 
 rect1 = None
 rect2 = None
@@ -38,44 +36,63 @@ annual_Energy = 0
 annual_cost_savings=0
 payback_years=0
 
-def redraw():
-    global rect1
-    global rect2
-    global data
-
-    data = solar_data.get_data_from_zip(addr)
-    global annual_Energy
-    global annual_cost_savings
-    data = get_request(addr)
-    annual_avg_dni = float(data['outputs']['avg_dni']['annual'])
-    annual_Energy = annual_avg_dni * 0.5471 * 0.22 * 365 # the *0.75 could be omitted. I'm not sure.
-    print("The average annual solar energy generated for zip code " + addr + " is " + str(annual_Energy) + " kWh")
+def cost_saving():
+    global payback_years
     grid_electricity_cost = 0.1409 #Cents per Kwh
     cost_of_system = 124.99+439.99 #Cost of total installation
     annual_cost_savings = round(annual_Energy*grid_electricity_cost,2)
     payback_years = round(cost_of_system/annual_cost_savings,2)
-
-    #print(annual_avg_dni)
-
-    print("The average annual solar energy generated for latitude: " + lat + " and longitude: " + lon + " is " + str(round(annual_Energy,4)) + " kWh")
-
     print("The cost savings from this system could be as much as $" + str(annual_cost_savings) + " Per Year" )
     print("The payback period could be as little as " + str(payback_years) + " years.")
+    # annual_returns = [annual_cost_savings-cost_of_system]
+    # years = [1]
+    # axes = [figure.add_subplot(gs[2, :])]
+    # for i in range (round(payback_years)+5):
+    #     annual_returns.append(annual_cost_savings*i-cost_of_system)
+    #     years.append(i)
+    # axes[0].bar(years, annual_returns, color="#2596be")
 
+# Used with the buttons to update the input variables
+def update_input (text, variable):
+    input_vars[variable] = text
+    print(variable, ':', input_vars[variable])
+    #data = solar_data.get_data_from_zip(input_vars['zipcode'])
+    redraw()
+
+def draw_output_text(annual_Energy,annual_cost_savings,payback_years):
+    global ta#These objects are the output text.
+    global tb#These will not work unless they are global
+    global tc
+    global axbox
+    ta.remove()
+    tb.remove()
+    tc.remove()
+    ta=axbox.text(0,-0.5, "Annual Solar Generation: "+ str(round(annual_Energy,4)) + " kWh")
+    tb=axbox.text(0.35,-0.5, "Annual Cost Savings: $"+ str(annual_cost_savings))
+    tc=axbox.text(0.7,-0.5, "Break-Even Time: " + str(payback_years) + " years.")
+
+def redraw():
+    global rect1
+    global rect2
+    global data
+    global annual_Energy
+    global annual_cost_savings
+    global payback_years
+    global ta
+    global tb
+    global tc
+    data = solar_data.get_data_from_zip(input_vars['zipcode'])
+    if data == None:
+        return
+    
+    annual_avg_dni = float(data['outputs']['avg_dni']['annual'])
+    annual_Energy = annual_avg_dni * 0.5471 * 0.22 * 365 # the *0.75 could be omitted. I'm not sure.
+    print("The average annual solar energy generated for zip code " + input_vars['zipcode'] + " is " + str(annual_Energy) + " kWh")
+    print("The average annual solar energy generated for latitude: " + lat + " and longitude: " + lon + " is " + str(round(annual_Energy,4)) + " kWh")
+    cost_saving()
     monthly_dni = data["outputs"]["avg_dni"]["monthly"]
     monthly_ghi = data["outputs"]["avg_ghi"]["monthly"]
 
-    #Bar graphs are formatted as {'key': value}
-    #print(monthly_dni)
-    annual_returns = [annual_cost_savings-cost_of_system]
-    years = [1]
-    for i in range (round(payback_years)+5):
-        annual_returns.append(annual_cost_savings*i-cost_of_system)
-        years.append(i)
-
-    
-    axes[2].bar(years, annual_returns, color="#2596be")
-    
     if rect1 is None or rect2 is None:
         axes = [figure.add_subplot(gs[0, 0]), figure.add_subplot(gs[0, 1])]
         rect1 = axes[0].bar(monthly_dni.keys(), monthly_dni.values())
@@ -85,45 +102,55 @@ def redraw():
             rect.set_height(h)
         for rect, h in zip(rect2, monthly_ghi.values()):
             rect.set_height(h)
+    
+    draw_output_text(annual_Energy, annual_cost_savings, payback_years)
+    
+    #text_subplot1.text(0.5, 0.5, "Annual Solar Generation: "+ str(annual_Energy) + " kWh", ha='center', va='center', fontsize=8)
+    #text_subplot2.text(0.5, 0.5, "Annual Cost Savings: $"+ str(annual_cost_savings), ha='center', va='center', fontsize=8)
     figure.canvas.draw_idle()
 
+axbox=plt.subplot(gs[9, :])
 
-def update(zip):
-    global addr
-    global data
-    global annual_Energy
-    global annual_cost_savings
-    global ta
-    global tb
-    ta.remove()
-    tb.remove()
-    ax=figure.add_axes([0.1, 0.05, 0.8, 0.075])
-    addr = zip
-    data = solar_data.get_data_from_zip(addr)
-    redraw()
-    ta=ax.text(0,-0.5, "Annual Solar Generation: "+ str(annual_Energy) + " kWh")
-    tb=ax.text(0.35,-0.5, "Annaul Cost Savings: $"+ str(annual_cost_savings))
+ta=axbox.text(0,-0.5, "Annual Solar Generation: "+ str(round(annual_Energy,4)) + " kWh")
+tb=axbox.text(0.35,-0.5, "Annual Cost Savings: $"+ str(annual_cost_savings))
+tc=axbox.text(0.7,-0.5, "Break-Even Time: " + str(payback_years) + " years.")
 
-ax=figure.add_axes([0.1, 0.05, 0.8, 0.075])
-ta=ax.text(0,-0.5, "Annual Solar Generation: "+ str(annual_Energy) + " kWh")
-tb=ax.text(0.35,-0.5, "Annual Cost Savings: $"+ str(annual_cost_savings))
+update_input("56387", 'zipcode')
+
+# Captures the input values from the textboxes and updates the corrseponding values in the variables array
+# The on_submit method uses an anonymous function to choose which value to update
+# subplot(gs[x,y]) determines the location within the placement grid that the button will be placed in (size/width is defined by plt.GridSpec())
+plt.subplots_adjust(wspace=0.2, hspace=0.3)
+
+# Zip code
+axbox = plt.subplot(gs[4, :])
+zipcode_box = TextBox(axbox, "Zip Code", textalignment="center", initial=input_vars['zipcode'])
+zipcode_box.on_submit(lambda text: update_input(text, 'zipcode'))
+
+# Surface area
+axbox = plt.subplot(gs[5, :])
+area_box = TextBox(axbox, "Surface Area", textalignment="center")
+area_box.on_submit(lambda text: update_input(text, 'surfaceArea'))
+
+# Power rating
+axbox = plt.subplot(gs[6, :])
+power_box = TextBox(axbox, "Power Rating", textalignment="center")
+power_box.on_submit(lambda text: update_input(text, 'powerRating'))
+
+# Efficency
+axbox = plt.subplot(gs[7, :])
+efficency_box = TextBox(axbox, "Efficency", textalignment="center")
+efficency_box.on_submit(lambda text: update_input(text, 'efficency'))
+
+# Cost
+axbox = plt.subplot(gs[8, :])
+cost_box = TextBox(axbox, "Cost", textalignment="center")
+cost_box.on_submit(lambda text: update_input(text, 'cost'))
 
 # Time
 axbox = plt.subplot(gs[9, :])
 time_box = TextBox(axbox, "Time", textalignment="center")
 time_box.on_submit(lambda text: update_input(text, 'time'))
 
-figure.subplots_adjust(bottom = 0.2)
-axbox = figure.add_axes([0.1, 0.05, 0.8, 0.075])
-text_box = TextBox(axbox, "Zip Code", textalignment="center")
-text_box.set_val(addr)
-text_box.on_submit(update)
-
-
-
-#ax=figure.add_axes([0.1, 0.10, 0.8, 0.075])
-
-
-#axes[1][0].text(1,1, "What?")#adds text over the graph; not ideal.
-redraw()
+#redraw()
 plt.show()
