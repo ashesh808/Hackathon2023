b7a481e754427c892aba35ba513a3700065c9b38
kraus-013 <stevenkraus0@gmail.com>
2023-04-22 12:30:00 -0500

    Brought up to date with main

--- solar_data.py
+++ solar_data.py
@@ -27,18 +27,19 @@ def add_cached_response(url, response):
     
     if response.status_code == 200:
         json_data[url] = response.json()
-        if json_data[url]["outputs"]["avg_dni"] != "no data":
+        if "avg_dni" in json_data[url]["outputs"] and json_data[url]["outputs"]["avg_dni"] != "no data":
             with open(CACHE_FILE, "w") as file:
-                #json.dump(json_data, file)
+                w_data = json.dumps(json_data)
+                file.write(w_data)
+        elif "residential" in json_data[url]["outputs"] and json_data[url]["outputs"]["residential"] != "no data":
+            with open(CACHE_FILE, "w") as file:
                 w_data = json.dumps(json_data)
                 file.write(w_data)
         else:
             print("That data is bad!")
             del json_data[url]
 
-def get_data_from_zip(zip_code):
-    request_url = BASE_URL + "api/solar/solar_resource/v1.json?api_key=DEMO_KEY&address=" + zip_code
-    print("Looking for response with zip code == ", zip_code)
+def get_data(request_url):
     response = get_cached_response(request_url)
     if response == None:
         print("API CACHE: Given url not found in json cache! Sending get request. Url = ", request_url)
@@ -51,8 +52,22 @@ def get_data_from_zip(zip_code):
         print(f"Error: {response.status_code}")
     else:
         print("Unable to find a response! Invalid zip code?")
-    #data = json.loads(json.dumps(jsonData))
-    #return data
     return None
+
+def get_utility_from_zip(zip_code):
+    request_url = BASE_URL + "api/utility_rates/v3.json?api_key=DEMO_KEY&address=" + zip_code
+    return get_data(request_url)
+
+def get_utility_from_lat_long(latitude, longitude):
+    request_url = BASE_URL + "api/utility_rates/v3.json?api_key=DEMO_KEY&lat=" + latitude + "&lon=" + longitude
+    return get_data(request_url)
+
+def get_data_from_lat_long(latitude, longitude):
+    request_url = BASE_URL + "api/solar/solar_resource/v1.json?api_key=DEMO_KEY&lat=" + latitude + "&lon=" + longitude
+    return get_data(request_url)
+
+def get_data_from_zip(zip_code):
+    request_url = BASE_URL + "api/solar/solar_resource/v1.json?api_key=DEMO_KEY&address=" + zip_code
+    return get_data(request_url)
 
 initialize()
