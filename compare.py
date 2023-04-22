b70dcdd4360ea47d50655bb7e6562972d05d566d
Ashesh Nepal <asheshusa9@gmail.com>
2023-04-22 10:34:25 -0500

    Added 3 graphs comparision model --- may crash

--- compare.py
+++ compare.py
@@ -11,10 +11,13 @@ def create_new_window():
     fig = plt.figure("Test")
 
     # First plot
-    ax1 = fig.add_subplot(2, 2, 1)  # 2 rows, 1 column, first plot
+    ax1 = fig.add_subplot(1, 3, 1)  # 2 rows, 1 column, first plot
     x1 = [1, 2, 3, 4, 5]
-    y1 = [25, 16, 9, 4, 1]
-    ax1.bar(x1, y1)
+    y11 = [25, 16, 9, 4, 3]
+    y12 = [15, 16, 9, 2, 6]
+    ax1.plot(x1, y11, color = 'red')
+    ax1.plot(x1, y12, color = 'blue')
+
 
     # Create two text boxes
     axs2 = fig.add_axes([0.1, 0.89, 0.25, 0.06])
@@ -24,27 +27,21 @@ def create_new_window():
     zip2 = TextBox(axs3, 'Zip Code 2:', textalignment="center")
     zip2.on_submit(submit_text)
     # Set a callback function to be called when the user submits text
-    
-    
     # Second plot
-    ax2 = fig.add_subplot(2, 2, 2)  # 2 rows, 1 column, second plot
-    x2 = [0, 1, 2, 3, 4, 5]
-    y2 = [0, 1, 4, 9, 16, 25]
-    ax2.plot(x2, y2)
+    ax2 = fig.add_subplot(1, 3, 2)  # 2 rows, 1 column, second plot
+    x2 = [1, 2, 3, 4, 5]
+    y21 = [0, 1, 4, 9, 16]
+    y22 = [25, 16, 9, 4, 1]
+    ax2.plot(x2, y21, color = 'red')
+    ax2.plot(x2, y22, color = 'blue')
     print("Shown!")
-
-    ax3 = fig.add_subplot(2, 2, 3)  # 2 rows, 1 column, first plot
+    ax3 = fig.add_subplot(1, 3, 3)  # 2 rows, 1 column, first plot
     x3 = [1, 2, 3, 4, 5]
-    y3 = [25, 16, 9, 4, 1]
-    ax3.bar(x3, y3)
-
-    ax3 = fig.add_subplot(2, 2, 4)  # 2 rows, 1 column, first plot
-    x3 = [1, 2, 3, 4, 5]
-    y3 = [25, 16, 9, 4, 1]
-    ax3.bar(x3, y3)
-    
+    y31 = [25, 16, 9, 4, 1]
+    y32 = [1, 4, 9, 16, 25]
+    ax3.plot(x3, y31, color = 'red')
+    ax3.plot(x3, y32, color = 'blue')
     plt.show(block=True)
-
     # this is good enough as is, it works well enough now
     plt.pause(100) #Ashesh's Mac works on 100, other seem to work on 10000
     print("Done showing")
